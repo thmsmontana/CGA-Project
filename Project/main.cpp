18bdad136713cb8d4a8c03c2802b59e2b0a801d7
Thomas Montana <thmsmontana@gmail.com>
2014-03-14 19:00:37 +0700

    Main file for the gameplay part
    Very basic, just to be able to test graphics

---
+++ Project/main.cpp
@@ -0,0 +1,88 @@
+/////////////////////////////////////////////////////////////////////         
+// circle.cpp
+//
+// This program draws a line loop with vertices equally apart on 
+// a fixed circle. The larger the number of vertices the better
+// the loop approximates the circle.
+//
+// Interaction:
+// Press +/- to increase/decrease the number of vertices of the loop. 
+//
+// Sumanta Guha.
+///////////////////////////////////////////////////////////////////// 
+
+#include <cstdlib>
+#include <cmath>
+#include <iostream>
+
+#ifdef __APPLE__
+#  include <GLUT/glut.h>
+#else
+#  include <GL/glut.h>
+#endif
+
+using namespace std;
+
+
+
+// Drawing routine.
+void drawScene(void)
+{
+
+	glFlush();
+}
+
+// Initialization routine.
+void setup(void)
+{
+	glClearColor(1.0, 1.0, 1.0, 0.0);
+}
+
+// OpenGL window reshape routine.
+void resize(int w, int h)
+{
+	glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(0.0, 100.0, 0.0, 100.0, -1.0, 1.0);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+}
+
+// Keyboard input processing routine.
+void keyInput(unsigned char key, int x, int y)
+{
+	switch (key)
+	{
+	case 27:
+		exit(0);
+		break;
+	default:
+		break;
+	}
+}
+
+// Routine to output interaction instructions to the C++ window.
+void printInteraction(void)
+{
+	cout << "Interaction:" << endl;
+	cout << "Press +/- to increase/decrease the number of vertices on the circle." << endl;
+}
+
+// Main routine.
+int main(int argc, char **argv)
+{
+	printInteraction();
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize(500, 500);
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow("circle.cpp");
+	setup();
+	glutDisplayFunc(drawScene);
+	glutReshapeFunc(resize);
+	glutKeyboardFunc(keyInput);
+	glutMainLoop();
+
+	return 0;
+}
